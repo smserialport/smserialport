564e5d87baee2a8a3c5f8829aae7ee7ee377861c
imba97 <mail@imba97.cn>
2024-04-20 23:20:09 +0800

    fix: `MainlandChinaHandleReturn` 类型导出问题

--- packages/smserialport/src/index.ts
+++ packages/smserialport/src/index.ts
@@ -2,7 +2,4 @@ export { Adapter } from '@smserialport/types'
 export type { AdapterOptinos } from '@smserialport/types'
 
 export { default as SMSerialport } from '@smserialport/core'
-export {
-  MainlandChinaAdapter,
-  MainlandChinaHandleReturn
-} from '@smserialport/adapters'
+export { MainlandChinaAdapter } from '@smserialport/adapters'
